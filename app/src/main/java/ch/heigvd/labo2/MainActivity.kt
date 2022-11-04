d70b67c4c77b45dfd4e47c63cbee335490dd79e8
Maxim Golay <m@xim.gy>
2022-11-04 10:07:17 +0100

    Added behavior on radio button check

--- app/src/main/java/ch/heigvd/labo2/MainActivity.kt
+++ app/src/main/java/ch/heigvd/labo2/MainActivity.kt
@@ -2,10 +2,31 @@ package ch.heigvd.labo2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioGroup
+import androidx.constraintlayout.widget.Group
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set interaction of radio buttons
+        val radioGroup = findViewById<RadioGroup>(R.id.occupation)
+        val studentGroup = findViewById<Group>(R.id.student_data)
+        val workerGroup = findViewById<Group>(R.id.worker_data)
+
+        radioGroup.setOnCheckedChangeListener { _, id ->
+            when (id) {
+                R.id.student -> {
+                    studentGroup.visibility = View.VISIBLE
+                    workerGroup.visibility = View.GONE
+                }
+                R.id.employee -> {
+                    studentGroup.visibility = View.GONE
+                    workerGroup.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
