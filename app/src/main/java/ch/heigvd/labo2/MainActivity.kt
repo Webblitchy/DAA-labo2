cbc5f1f39aaa226073bfa37fc94e03baa516f666
Luhrel <57545107+Luhrel@users.noreply.github.com>
2022-11-15 07:44:27 +0100

    Use Log.println instead of println

--- app/src/main/java/ch/heigvd/labo2/MainActivity.kt
+++ app/src/main/java/ch/heigvd/labo2/MainActivity.kt
@@ -6,6 +6,7 @@ import android.icu.text.SimpleDateFormat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
@@ -353,7 +354,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             // On log la personne
-            println(person.toString())
+            Log.println(Log.INFO, null, person.toString())
 
             // Message pour l'utilisateur
             Toast
